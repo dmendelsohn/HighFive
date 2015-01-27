860a050f236183cc1061b0a61c0b9747cc72a74d
scottviteri <scottviteri@gmail.com>
2015-01-27 02:28:53 -0500

    fixed some silly stuff

--- RobotCode/src/states/WallFollowTest.java
+++ RobotCode/src/states/WallFollowTest.java
@@ -1,12 +1,12 @@
 package states;
 import robot.*;
 
-public class WallFollow extends StateBase{
+public class WallFollowTest extends StateBase{
 
     public OutputStateVariables output;
     public InputStateVariables input;
 
-    public WallFollow(){
+    public WallFollowTest(){
 
 	super();
 
@@ -23,20 +23,14 @@ public class WallFollow extends StateBase{
     }
 
     public OutputStateVariables run(InputStateVariables input){
-	System.out.println("WallFollow");
+	System.out.println("WallFollowTest");
+	if (System.currentTimeMillis()-stateStartTime>10000){
+	    output.drivetrainMethod = "stop";
+	}
 	return output;
     }
 
     public StateBase getNext(InputStateVariables input){
-	//if(input.seesTarget){
-	if(false){
-	    return new BoxFollow();
-	}else if(System.currentTimeMillis()-stateStartTime > 4000.){
-	    return new BoxSearch();
-	}else if(input.frontUltraDist<0.1){
-	    return new WallTurn();
-	}else{
-	    return this;
-	}
+	return this;
     }
 }
