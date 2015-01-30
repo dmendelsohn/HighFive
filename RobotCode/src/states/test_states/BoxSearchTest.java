729df4d929031f83a3669224ae0a1dcea064b62e
scottviteri <scottviteri@gmail.com>
2015-01-30 00:59:38 -0500

    added separate box processing wait class

--- RobotCode/src/states/test_states/BoxSearchTest.java
+++ RobotCode/src/states/test_states/BoxSearchTest.java
@@ -18,23 +18,16 @@ public class BoxSearchTest extends StateBase{
 
     public OutputStateVariables run(InputStateVariables input){
 	OutputStateVariables output = getDefaultOutput();
-	if(super.getElapsedTime()%5000<2500){
-	    output.driveTrainMethod = DriveTrainMethod.STOP;
-	}else{
-	    output.driveTrainMethod = DriveTrainMethod.PID_DRIVE;
-	    output.driveTrainSpeed = 0.0;
-	    output.driveTrainPidAngle = -360.0;
-	}
 	return output;
     }
 
     public StateBase getNext(InputStateVariables input){
 	if(input.seesTarget){
 	    return new VisionTest();
-	}else if(input.gyroAngle>360.0){
-	    return new VisionTest();
-	}else if(super.getElapsedTime()>15000){
+	}else if(input.gyroAngle>360.){
 	    return new FindWallTest();
+	}else if(super.getElapsedTime()>2500){
+	    return new FindBoxTest();
 	} else{
 	    return this;
 	}
