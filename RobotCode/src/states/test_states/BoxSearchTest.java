559367050193afe10658cc50abb672464a48ed74
scottviteri <scottviteri@gmail.com>
2015-01-30 00:42:03 -0500

    Increased box search wait and changed search direction

--- RobotCode/src/states/test_states/BoxSearchTest.java
+++ RobotCode/src/states/test_states/BoxSearchTest.java
@@ -9,21 +9,21 @@ public class BoxSearchTest extends StateBase{
     @Override
     public OutputStateVariables getDefaultOutput() {
 	OutputStateVariables output = super.getDefaultOutput();
-	output.zeroGyro = true;
+	//output.zeroGyro = true;
 	output.driveTrainMethod = DriveTrainMethod.PID_DRIVE;
 	output.driveTrainSpeed = 0.0;
-	output.driveTrainPidAngle = 360.0;
+	output.driveTrainPidAngle = -360.0;
 	return output;
     }
 
     public OutputStateVariables run(InputStateVariables input){
 	OutputStateVariables output = getDefaultOutput();
-	if(super.getElapsedTime()%2500<100){
+	if(super.getElapsedTime()%5000<2500){
 	    output.driveTrainMethod = DriveTrainMethod.STOP;
 	}else{
 	    output.driveTrainMethod = DriveTrainMethod.PID_DRIVE;
 	    output.driveTrainSpeed = 0.0;
-	    output.driveTrainPidAngle = 360.0;
+	    output.driveTrainPidAngle = -360.0;
 	}
 	return output;
     }
@@ -31,7 +31,9 @@ public class BoxSearchTest extends StateBase{
     public StateBase getNext(InputStateVariables input){
 	if(input.seesTarget){
 	    return new VisionTest();
-	}else if(super.getElapsedTime()>10000.){
+	}else if(input.gyroAngle>360.0){
+	    return new VisionTest();
+	}else if(super.getElapsedTime()>15000){
 	    return new FindWallTest();
 	} else{
 	    return this;
